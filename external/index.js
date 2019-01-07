283166c85730ec01a93f19546910804c45a8a141
Kenneth Chau <Kenneth.Chau@massive.co>
2019-01-07 13:35:29 +0000

    use axios

--- external/index.js
+++ external/index.js
@@ -1,13 +1,20 @@
 const app = require('express')();
 
-const DELAY = process.env.DELAY || 1000;
 const PORT = process.env.PORT || 3000;
+const DELAY = parseInt(process.env.DELAY) || 1000;
+const INCREMENT = parseInt(process.env.INCREMENT)|| 1;
 
 let i = DELAY;
 
 app.get('/health', (_, res) => res.end("ok"));
 app.get('/*', (req, res) => {
-    setTimeout(() => res.end("done"), i++)
+    req.on('error', err => {
+        console.log(err);
+    })
+    req.on('close', () => {
+        console.log('connection closed');
+    })
+    setTimeout(() => res.end("done"), i += INCREMENT)
 });
 
 app.listen(PORT, () => console.log('external started'));
--- server/index.js
+++ server/index.js
@@ -1,19 +1,24 @@
-const http = require('http');
+const axios = require('axios').default;
+const circularJson = require('circular-json');
 const app = require('express')();
 
 const EXTERNAL_HOST_NAME = process.env.EXTERNAL_HOST_NAME;
 const PORT = process.env.PORT || 3000;
 
+const timeout = parseInt(process.env.EXT_TIMEOUT, 10) || 3100;
+
 app.get('/health', (_, res) => res.end("ok"));
 app.get('/*', (req, res) => {
-    extReq = http.get(EXTERNAL_HOST_NAME, extRes => {
+    return axios.get(EXTERNAL_HOST_NAME, {timeout}).then(extRes => {
         res.end("done");
-    })
-    extReq.on('error', err => {
-        console.log(err)
+    }).catch(err => {
+        const out = (({code, status, data}) => ({code, status, data}))(err);
+        console.log(out);
+
         res.status(500);
-        res.end();
-    });
+        res.setHeader('Content-Type', 'application/json');
+        res.end(JSON.stringify(out));
+    })
 });
 
 app.listen(PORT, () => console.log('server started'));
